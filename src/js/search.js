3260a71e76108d927ecce8fcb7318743fa0f7fab
Hanin Farhan <haninfarhan96@gmail.com>
2024-03-06 12:21:43 +0100

    Ändrat bild, lagt till funktion för att skriva ut data, ändrat typsnitt + små css justeringar

--- src/js/search.js
+++ src/js/search.js
@@ -3,28 +3,53 @@
 let searchBtn = document.getElementById("search-btn");
 
 //Lägger till eventlistener vid klick på sökknapp
-searchBtn.addEventListener("click", searchCountry);
+searchBtn.addEventListener("click", displayCountry);
 
-
+//Hämtar api för restcountries
 async function searchCountry() {
     let searchInput = document.getElementById("recipe-search").value;
 
 
     //Hämtar specifik land api från Restcountries
-    const url = "https://restcountries.com/v3.1/name/" + searchInput;
+    const url = "https://restcountries.com/v3.1/name/" + searchInput + "?fullText=true";
 
     //Kör fetch på url som returnerar en promise
     try {
         let response = await fetch(url);
+
         let data = await response.json();
         console.log(data);
+        
+        return data[0];//Hämtar första index
+
 
     } catch (error) {
         console.error(error); //Vid fel körs error meddelande i konsollen
+        return null;
     }
 }
 
+//Funktion som skriver ut datan i form av flagga
+async function displayCountry() {
+    document.getElementById("flag").innerHTML = "";
 
-async function displayFlags() {
-    
+    let country = await searchCountry();
+    document.querySelector(".flag-wrap").style.display = "block"; //Gör css stil för flag-wrap aktiv
+
+    if (country) {
+        document.getElementById("flag").innerHTML =
+            `<img src="${country.flags.svg}" alt="${country.flags.alt}">`; //Skriver ut flaga
+
+        document.querySelector(".country-name").innerHTML = `<p>"${countryName}"</p>`//Skriver ut land
+    }
+    else {
+        return document.getElementById("flag").innerHTML = "<p>Country not found</p>";
+    }
+
 }
+
+
+
+
+
+
